4a6aedf93d590de5f0062d1ff060605167b3b4d6
Wojciech Witek <wojtekwitek@gmail.com>
2017-02-18 08:10:29 +0100

    Added Mobile clients with CocosSharp and simple circle sample page

---
+++ src/Clients/Connect4.Mobile/Connect4.Mobile/App.cs
@@ -0,0 +1,35 @@
+﻿using Connect4.Mobile.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Connect4.Mobile
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var content = new HomePage();
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
