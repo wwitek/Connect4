1929b197c29cd23a5e99a47b22846ea1e4b56bbb
Wojciech Witek <wojtekwitek@gmail.com>
2017-02-28 04:31:01 +0100

    Added new ball colors

--- src/Clients/Connect4.Mobile/Connect4.Mobile/GameScene.cs
+++ src/Clients/Connect4.Mobile/Connect4.Mobile/GameScene.cs
@@ -20,13 +20,27 @@ namespace Connect4.Mobile
         {
             CCColor4B startColor = new CCColor4B(1, 36, 76);
             CCColor4B endColor = new CCColor4B(4, 79, 162);
-            CCColor4B circleBorder = new CCColor4B(2, 23, 50);
-            CCColor4B circleColor = new CCColor4B(178, 216, 255);
+
+            CCColor4B circleDarkerBorder = new CCColor4B(2, 24, 50);
+            CCColor4B circleLighterBorder = new CCColor4B(1, 36, 76);
+
+            CCColor4B circleDarkerColor = new CCColor4B(104, 172, 250);
+            CCColor4B circleLighterColor = new CCColor4B(178, 216, 255);
+
+
+            CCColor4B yellowColor = new CCColor4B(255, 211, 0);
+            CCColor4B royalYellowColor = new CCColor4B(250, 218, 94);
+            CCColor4B goldYellowColor = new CCColor4B(255, 215, 0);
+
+            CCColor4B redColor = new CCColor4B(196, 2, 51);
+            CCColor4B scarletRedColor = new CCColor4B(255, 36, 0);
+            CCColor4B fireEngineRed = new CCColor4B(206, 32, 41);
+
 
             _boardCoordinates = new CCPoint[7, 6];
             _viewWidth = gameView.DesignResolution.Width;
             _viewHeight = gameView.DesignResolution.Height;
-            _circleGap = (float)Math.Ceiling(_viewWidth / 7 * 0.05);
+            _circleGap = (float)Math.Ceiling(_viewWidth / 7 * 0.1);
             _circleSize = (float)Math.Floor((_viewWidth - (_circleGap * 10)) / 7);
             if (((_circleSize % 2) == 0 && (_viewWidth % 2) != 0) || ((_circleSize % 2) != 0 && (_viewWidth % 2) == 0))
             {
@@ -56,26 +70,70 @@ namespace Connect4.Mobile
                     circle.DrawSolidCircle(
                         new CCPoint(x + _circleSize / 2, y + _circleSize / 2),
                         radius: _circleSize / 2,
-                        color: circleColor);
+                        color: circleLighterColor);
+                    layer.AddChild(circle);
+
+                    circle.DrawEllipse(
+                        rect: new CCRect(x + 1, y + 1, _circleSize - 2, _circleSize - 2),
+                        lineWidth: 1,
+                        color: circleDarkerColor);
                     layer.AddChild(circle);
 
                     circle.DrawEllipse(
                         rect: new CCRect(x, y, _circleSize, _circleSize),
                         lineWidth: 1,
-                        color: circleBorder);
+                        color: circleLighterBorder);
                     layer.AddChild(circle);
                 }
             }
 
-            var ball = new CCSprite("redball");
-            ball.ContentSize = new CCSize(_circleSize, _circleSize);
-            ball.Position = _boardCoordinates[2, 3];
-            layer.AddChild(ball);
+            //var ball = new CCSprite("redball");
+            //ball.ContentSize = new CCSize(_circleSize, _circleSize);
+            //ball.Position = _boardCoordinates[2, 3];
+            //layer.AddChild(ball);
+            
+            //var ball2 = new CCSprite("yellowball");
+            //ball2.ContentSize = new CCSize(_circleSize, _circleSize);
+            //ball2.Position = _boardCoordinates[6, 1];
+            //layer.AddChild(ball2);
 
-            var ball2 = new CCSprite("yellowball");
-            ball2.ContentSize = new CCSize(_circleSize, _circleSize);
-            ball2.Position = _boardCoordinates[6, 1];
-            layer.AddChild(ball2);
+            float ballRadius = (_circleSize / 2) - 1;
+
+            circle.DrawSolidCircle(
+                _boardCoordinates[1, 1],
+                radius: ballRadius,
+                color: yellowColor);
+            layer.AddChild(circle);
+
+            circle.DrawSolidCircle(
+                _boardCoordinates[1, 2],
+                radius: ballRadius,
+                color: royalYellowColor);
+            layer.AddChild(circle);
+
+            circle.DrawSolidCircle(
+                _boardCoordinates[1, 4],
+                radius: ballRadius,
+                color: goldYellowColor);
+            layer.AddChild(circle);
+
+            circle.DrawSolidCircle(
+                _boardCoordinates[3, 2],
+                radius: ballRadius,
+                color: redColor);
+            layer.AddChild(circle);
+
+            circle.DrawSolidCircle(
+                _boardCoordinates[3, 3],
+                radius: ballRadius,
+                color: scarletRedColor);
+            layer.AddChild(circle);
+
+            circle.DrawSolidCircle(
+                _boardCoordinates[3, 5],
+                radius: ballRadius,
+                color: fireEngineRed);
+            layer.AddChild(circle);
         }
     }
 }
